d801bf98531b10ad4adb5d8c8e67ce6c337df116
deathlotuspenta <deathlotuspenta@outlook.com>
2025-12-19 15:53:48 +0800

    day2 学会函数式更新

--- src/App.js
+++ src/App.js
@@ -9,8 +9,11 @@ function App() {
   const [showList,setShowList] = useState(false);
 
 
-function changeIsHidden(value) {
-  setShowList(!value);
+function changeIsHidden() {
+  // react中状态更新不是立刻生效的，是异步的
+  // 所以不能通过showList的值来更新showList setShowList(!showList) 这样写是错误的 如果连续调用两次 则结果会出错
+  // 推荐使用函数式的写法 如果不依赖旧值可以直接写成setShowList(true or false) 如果依赖旧值则写成setShowList(prev => !prev)
+  setShowList(prev => !prev);
 }
 
   return (
